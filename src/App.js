f2bed12d37c632bf9fefc88bd84628f6f8513e54
Francesca Vago <vago.francesca@gmail.com>
2019-10-31 14:19:25 +0000

    create joke component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bar from './bar.jpg';
+import geri from './geri.jpg';
+import farage from './farage.jpg';
+
+function BrexitJoke(props) {
+  return (
+    <div>
+      <img src={props.img} alt=" " />
+      <h2>{props.text}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome the Brexit Jokes App!</h1>
+      <BrexitJoke
+        text="Joke 1"
+        img={bar} />
+      <BrexitJoke
+        text="Joke 2"
+        img={geri} />
+      <BrexitJoke
+        text="Joke 3"
+        img={farage} />
     </div>
   );
 }
